f0cf889b8352b3d5ffd41d42659c81b26c64cf06
danielohayon420 <danielohayon420@gmail.com>
2023-03-10 15:13:58 +0200

    first commit - UI views

---
+++ README.md
@@ -0,0 +1,1 @@
+# Have_We_Met
---
+++ app/src/main/java/com/project/havewemet/MainActivity.java
@@ -0,0 +1,39 @@
+package com.project.havewemet;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.internal.NavigationMenu;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        NavHostFragment navHostFragment = (NavHostFragment)getSupportFragmentManager().findFragmentById(R.id.main_navhost);
+
+        ImageView ivMenu = findViewById(R.id.iv_menu);
+        ivMenu.setOnClickListener(view -> {
+            PopupMenu popupMenu = new PopupMenu(this, view);
+            popupMenu.inflate(R.menu.main_menu);
+            popupMenu.setOnMenuItemClickListener(menuItem -> {
+                return true;
+            }); // end of popup menu click listener
+            popupMenu.show();
+        }); //end of ivMenu click listener,(lambda)
+
+    }
+}
